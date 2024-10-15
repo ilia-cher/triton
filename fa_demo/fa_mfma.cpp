4223c5e4d0e2e4dcbbbec6975cf826495f8047c6
Ilia C <Ilia.Cherniavskii@amd.com>
2024-10-15 14:33:52 -0500

    LDS prefetch + N=64 (FA)
    ```
    remark: fa_mfma.cpp:10:0: Function Name: _Z7fa_mfmaPKDF16_S0_S0_PDF16_ [-Rpass-analysis=kernel-resource-usage]
    remark: fa_mfma.cpp:10:0:     SGPRs: 30 [-Rpass-analysis=kernel-resource-usage]
    remark: fa_mfma.cpp:10:0:     VGPRs: 198 [-Rpass-analysis=kernel-resource-usage]
    remark: fa_mfma.cpp:10:0:     AGPRs: 0 [-Rpass-analysis=kernel-resource-usage]
    remark: fa_mfma.cpp:10:0:     ScratchSize [bytes/lane]: 0 [-Rpass-analysis=kernel-resource-usage]
    remark: fa_mfma.cpp:10:0:     Dynamic Stack: False [-Rpass-analysis=kernel-resource-usage]
    remark: fa_mfma.cpp:10:0:     Occupancy [waves/SIMD]: 2 [-Rpass-analysis=kernel-resource-usage]
    remark: fa_mfma.cpp:10:0:     SGPRs Spill: 0 [-Rpass-analysis=kernel-resource-usage]
    remark: fa_mfma.cpp:10:0:     VGPRs Spill: 0 [-Rpass-analysis=kernel-resource-usage]
    remark: fa_mfma.cpp:10:0:     LDS Size [bytes/block]: 65536 [-Rpass-analysis=kernel-resource-usage]
    
    Creating empty rpd: results.rpd
    rpd_tracer, because
    Initializing tensors... done
    
    FP16 FA: Nctx, Dhead = 16384, 128; BH = 16
    Computing ref... done
    
    attn_ref/ref :  OK
    naive/ref    :  OK
    mfma/ref     :  OK
    rocpd_op: 0
    rocpd_api_ops: 0
    rocpd_kernelapi: 0
    rocpd_copyapi: 0
    rocpd_api: 0
    rocpd_string: 0
    rpd_tracer: finalized in 7.248019 ms
                                 Name  TotalCalls  TotalDurationUs  AvgDurationUs
    0  _Z8fa_naivePKDF16_S0_S0_PDF16_          20      9039965.413   451998.27065
    1   _Z7fa_mfmaPKDF16_S0_S0_PDF16_          20       127543.832     6377.19160
    ```

--- fa_demo/fa_mfma.cpp
+++ fa_demo/fa_mfma.cpp
@@ -19,10 +19,10 @@ __global__ void fa_mfma(
   const float16_t *K_stripe = K;
   const float16_t *V_stripe = V;
 
-  __shared__ float16_t KS[32 * 8 * FA_MFMA_K_BLOCKS];
-  __shared__ float16_t KS_next[32 * 8 * FA_MFMA_K_BLOCKS];
-  __shared__ float16_t VS[8 * 32 * FA_MFMA_K_BLOCKS];
-  __shared__ float16_t VS_next[8 * 32 * FA_MFMA_K_BLOCKS];
+  __shared__ float16_t KS[32 * 8 * FA_MFMA_K_BLOCKS * 2];
+  __shared__ float16_t KS_next[32 * 8 * FA_MFMA_K_BLOCKS * 2];
+  __shared__ float16_t VS[8 * 32 * FA_MFMA_K_BLOCKS * 2];
+  __shared__ float16_t VS_next[8 * 32 * FA_MFMA_K_BLOCKS * 2];
 
   float16x4 Q_tile_reg[FA_MFMA_K_BLOCKS];
   const float16_t *Q_tile = Q_stripe;
@@ -36,112 +36,123 @@ __global__ void fa_mfma(
   floatx16 accO[4] = {{0.0}};
 
   //
-  float16x8 K_stripe_reg;
-  mfma_load_K_stripe_to_reg(K_stripe_reg, K_stripe);
+  float16x8 K_stripe_reg[2];
+  mfma_load_K_stripe_to_reg(K_stripe_reg[0], K_stripe);
+  mfma_load_K_stripe_to_reg(K_stripe_reg[1], K_stripe + K_IDX(32, 0));
 
   float16_t *KS_stripe = KS;
   float16_t *KS_stripe_next = KS_next;
-  mfma_store_reg_to_KS_stripe(K_stripe_reg, KS_stripe);
+  mfma_store_reg_to_KS_stripe(K_stripe_reg[0], KS_stripe);
+  mfma_store_reg_to_KS_stripe(K_stripe_reg[1], KS_stripe + _KS_IDX(16, 0, 0));
 
-  float16x8 V_stripe_reg;
-  mfma_load_V_stripe_to_reg(V_stripe_reg, V_stripe);
+  float16x8 V_stripe_reg[2];
+  mfma_load_V_stripe_to_reg(V_stripe_reg[0], V_stripe);
+  mfma_load_V_stripe_to_reg(V_stripe_reg[1], V_stripe + V_IDX(32, 0));
 
   float16_t *VS_stripe = VS;
   float16_t *VS_stripe_next = VS_next;
-  mfma_store_reg_to_VS_stripe(V_stripe_reg, VS_stripe);
+  mfma_store_reg_to_VS_stripe(V_stripe_reg[0], VS_stripe);
+  mfma_store_reg_to_VS_stripe(V_stripe_reg[1], VS_stripe + _VS_IDX(4, 0, 0, 0));
 
   __syncthreads();
   //
 
-  const int nBlocks = CEIL_DIV(NCTX, 32);
+  const int nBlocks = CEIL_DIV(NCTX, 64);
   for (int nBlockIdx = 0; nBlockIdx < nBlocks; ++nBlockIdx) {
     // Prefetch the next stripes of K and V from HBM into the registers
     if (nBlockIdx < nBlocks - 1) {
-      const float16_t *K_stripe_next = K_stripe + K_IDX(32, 0);
-      mfma_load_K_stripe_to_reg(K_stripe_reg, K_stripe_next);
+      const float16_t *K_stripe_next = K_stripe + K_IDX(64, 0);
+      mfma_load_K_stripe_to_reg(K_stripe_reg[0], K_stripe_next);
+      mfma_load_K_stripe_to_reg(K_stripe_reg[1], K_stripe_next + K_IDX(32, 0));
 
-      const float16_t *V_stripe_next = V_stripe + V_IDX(32, 0);
-      mfma_load_V_stripe_to_reg(V_stripe_reg, V_stripe_next);
+      const float16_t *V_stripe_next = V_stripe + V_IDX(64, 0);
+      mfma_load_V_stripe_to_reg(V_stripe_reg[0], V_stripe_next);
+      mfma_load_V_stripe_to_reg(V_stripe_reg[1], V_stripe_next + V_IDX(32, 0));
     }
 
     // First GEMM
+    floatx16 acc[2] = {{0.0}};
+    for (int nIter = 0; nIter < 2; ++nIter) {
+      // Preload K from LDS into the registers
+      float16x4 K_tile_reg[FA_MFMA_K_BLOCKS];
+      const float16_t *KS_tile = KS_stripe + _KS_IDX(nIter * 16, 0, 0);
+      for (int k = 0; k < FA_MFMA_K_BLOCKS; ++k) {
+        mfma_load_KS_tile_to_reg(K_tile_reg[k], KS_tile); KS_tile += _KS_IDX(1, 0, 0);
+      }
 
-    // Preload K from LDS into the registers
-    float16x4 K_tile_reg[FA_MFMA_K_BLOCKS];
-    const float16_t *KS_tile = KS_stripe;
-    for (int k = 0; k < FA_MFMA_K_BLOCKS; ++k) {
-      mfma_load_KS_tile_to_reg(K_tile_reg[k], KS_tile); KS_tile += _KS_IDX(1, 0, 0);
-    }
-
-    floatx16 acc = {0.0};
-    //const float16_t *KS_tile = KS_stripe;
-    for (int k = 0; k < FA_MFMA_K_BLOCKS; ++k) {
-      //float16x4 K_tile_reg;
-      //mfma_load_KS_tile_to_reg(K_tile_reg, KS_tile); KS_tile += _KS_IDX(1, 0, 0);
-      acc = __builtin_amdgcn_mfma_f32_32x32x8f16(K_tile_reg[k], Q_tile_reg[k], acc, 0, 0, 0);
+      for (int k = 0; k < FA_MFMA_K_BLOCKS; ++k) {
+        acc[nIter] = __builtin_amdgcn_mfma_f32_32x32x8f16(K_tile_reg[k], Q_tile_reg[k], acc[nIter], 0, 0, 0);
+      }
     }
 
     // Online softmax
-    float rowmax1 = -std::numeric_limits<float>::infinity();
-    for (int idx = 0; idx < 16; ++idx) {
-      if (rowmax1 < acc[idx]) {
-        rowmax1 = acc[idx];
+    float alpha = 1.0;
+    for (int nIter = 0; nIter < 2; ++nIter) {
+      float rowmax1 = -std::numeric_limits<float>::infinity();
+      for (int idx = 0; idx < 16; ++idx) {
+        if (rowmax1 < acc[nIter][idx]) {
+          rowmax1 = acc[nIter][idx];
+        }
       }
-    }
-    float rowmax2 = __shfl(rowmax1, (threadIdx.x + 32) % 64);
-    float rowmax = (rowmax1 > rowmax2) ? rowmax1 : rowmax2;
-    float Mnew = (rowmax > M) ? rowmax : M;
-    float alpha = std::exp2f(M - Mnew);
+      float rowmax2 = __shfl(rowmax1, (threadIdx.x + 32) % 64);
+      float rowmax = (rowmax1 > rowmax2) ? rowmax1 : rowmax2;
+      float Mnew = (rowmax > M) ? rowmax : M;
+      alpha *= std::exp2f(M - Mnew);
 
-    M = Mnew;
+      M = Mnew;
+    }
     L *= alpha;
 
-    float16x4 accP[4];
-    float rowsum1 = 0;
-    for (int idx = 0; idx < 16; ++idx) {
-      float e_acc = std::exp2f(acc[idx] - M);
-      rowsum1 += e_acc;
-      accP[idx / 4][idx % 4] = (float16_t)e_acc;
+    float16x4 accP[2][4];
+    for (int nIter = 0; nIter < 2; ++nIter) {
+      float rowsum1 = 0;
+      for (int idx = 0; idx < 16; ++idx) {
+        float e_acc = std::exp2f(acc[nIter][idx] - M);
+        rowsum1 += e_acc;
+        accP[nIter][idx / 4][idx % 4] = (float16_t)e_acc;
+      }
+      float rowsum2 = __shfl(rowsum1, (threadIdx.x + 32) % 64);
+      float rowsum = rowsum1 + rowsum2;
+      L += rowsum;
     }
-    float rowsum2 = __shfl(rowsum1, (threadIdx.x + 32) % 64);
-    float rowsum = rowsum1 + rowsum2;
-    L += rowsum;
 
     // Write the next stripes of K and V from the registers into LDS
     if (nBlockIdx < nBlocks - 1) {
-      mfma_store_reg_to_KS_stripe(K_stripe_reg, KS_stripe_next);
-      mfma_store_reg_to_VS_stripe(V_stripe_reg, VS_stripe_next);
+      mfma_store_reg_to_KS_stripe(K_stripe_reg[0], KS_stripe_next);
+      mfma_store_reg_to_KS_stripe(K_stripe_reg[1], KS_stripe_next + _KS_IDX(16, 0, 0));
+
+      mfma_store_reg_to_VS_stripe(V_stripe_reg[0], VS_stripe_next);
+      mfma_store_reg_to_VS_stripe(V_stripe_reg[1], VS_stripe_next + _VS_IDX(4, 0, 0, 0));
     }
 
     // Second GEMM
 
+    // Correct accO using the new running max
     for (int oIdx = 0; oIdx < 4; ++oIdx) {
-      // Correct accO using the new running max
       for (int accIdx = 0; accIdx < 16; ++accIdx) {
         accO[oIdx][accIdx] *= alpha;
       }
     }
 
-    // Preload V from LDS into the registers
-    float16x4 V_tile_reg[4][4];
-    const float16_t *VS_tile = VS_stripe;
-    for (int idx = 0; idx < 16; ++idx) {
-      mfma_load_VS_tile_to_reg(V_tile_reg[idx / 4][idx % 4], VS_tile); VS_tile += _VS_IDX(0, 1, 0, 0);
-    }
+    for (int nIter = 0; nIter < 2; ++nIter) {
+      // Preload V from LDS into the registers
+      float16x4 V_tile_reg[4][4];
+      const float16_t *VS_tile = VS_stripe + _VS_IDX(nIter * 4, 0, 0, 0);
+      for (int idx = 0; idx < 16; ++idx) {
+        mfma_load_VS_tile_to_reg(V_tile_reg[idx / 4][idx % 4], VS_tile); VS_tile += _VS_IDX(0, 1, 0, 0);
+      }
 
-    //const float16_t *VS_tile = VS_stripe;
-    for (int oIdx = 0; oIdx < 4; ++oIdx) {
-      for (int pIdx = 0; pIdx < 4; ++pIdx) {
-        //float16x4 V_tile_reg;
-        //mfma_load_VS_tile_to_reg(V_tile_reg, VS_tile); VS_tile += _VS_IDX(0, 1, 0, 0);
-        accO[oIdx] = __builtin_amdgcn_mfma_f32_32x32x8f16(V_tile_reg[oIdx][pIdx], accP[pIdx], accO[oIdx], 0, 0, 0);
+      for (int oIdx = 0; oIdx < 4; ++oIdx) {
+        for (int pIdx = 0; pIdx < 4; ++pIdx) {
+          accO[oIdx] = __builtin_amdgcn_mfma_f32_32x32x8f16(V_tile_reg[oIdx][pIdx], accP[nIter][pIdx], accO[oIdx], 0, 0, 0);
+        }
       }
     }
 
     __syncthreads();
 
-    K_stripe += K_IDX(32, 0);
-    V_stripe += V_IDX(32, 0);
+    K_stripe += K_IDX(64, 0);
+    V_stripe += V_IDX(64, 0);
     swap_ptr(KS_stripe, KS_stripe_next);
     swap_ptr(VS_stripe, VS_stripe_next);
   }  // nBlockIdx
